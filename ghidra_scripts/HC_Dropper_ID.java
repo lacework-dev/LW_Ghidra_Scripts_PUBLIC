93d6fc4a7ba5a8d398a7c05f956d9ed3bef64804
lw-jared <81973608+lw-jared@users.noreply.github.com>
2021-09-23 08:40:55 -0400

    Initial commit of HC Dropper ID

---
+++ ghidra_scripts/HC_Dropper_ID.java
@@ -0,0 +1,37 @@
+//
+//@author Lacework Labs
+//@category 
+//@keybinding
+//@menupath
+//@toolbar
+
+import ghidra.app.script.GhidraScript;
+import ghidra.program.model.address.Address;
+import ghidra.program.model.symbol.Reference;
+import ghidra.program.model.symbol.Symbol;
+
+public class HC_Dropper_ID extends GhidraScript {
+
+	
+	static String insmod_str = "/sbin/insmod";
+	static String ELF = "ELF";
+	
+	@Override
+	protected void run() throws Exception {
+		
+		Address insmodAddr = find(insmod_str);
+		println("[Dropper] insmod string identified at " + insmodAddr.toString());
+		Reference[] ref = getReferencesTo(insmodAddr);
+		Address insmodInMain = ref[0].getFromAddress();
+		println("[Dropper] insmod referenced at "+ insmodInMain.toString());
+		println("[Dropper - KO ELF] " + insmodInMain.subtract(14));
+		Symbol main =  getSymbolBefore(insmodAddr);
+		Address[] embeddedELFs = findBytes(main.getAddress(), ELF, 2);
+		
+		
+		for (int x = 0; x < embeddedELFs.length; x++) {
+			println("[+] Found embedded ELF " + embeddedELFs[x].toString());
+		}
+		
+	}
+}
